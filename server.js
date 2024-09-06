3fe674dbe31e0cb40095842eafba571f990cdb71
hosung8818 <naver.com>
2024-09-06 16:06:55 +0900

    1

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mysql = require("mysql2");
-const cors = require("cors"); // CORS 패키지 추가
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// CORS 설정
 app.use(cors());
 
 const connection = mysql.createConnection({
@@ -16,16 +15,17 @@ const connection = mysql.createConnection({
 
 app.get("/data", (req, res) => {
   const query = `
-    SELECT 
-        설계기능,
-        GROUP_CONCAT(DISTINCT 도면종류 ORDER BY 도면종류 SEPARATOR ', ') AS 도면종류_리스트
-    FROM 
-        three_line
-    GROUP BY 
-        설계기능
-    ORDER BY 
-        설계기능;
-`; //바꿀꺼
+    SELECT
+      년도,
+      월,
+      총계획수량,
+      총실적수량
+    FROM
+      monthly_summary2
+    ORDER BY
+      년도,
+      월;
+  `;
 
   connection.query(query, (error, results) => {
     if (error) {
